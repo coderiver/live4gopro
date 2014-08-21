16b31df5c9b8418f50b9764e4a447d1692c0fc39
akella <akella.a@gmail.com>
2014-08-22 01:54:34 +0300

    responsive shit

--- js/common.js
+++ js/common.js
@@ -3,6 +3,9 @@ head.ready(function() {
 	// $(document).on("click", function(){
 	// 	$(".js-popup").hide();
 	// });
+	$('.header__menu').click(function(event) {
+		$('.header ul').toggleClass('is-mobileshown')
+	});
 
 	console.log($('body').html());
 });
